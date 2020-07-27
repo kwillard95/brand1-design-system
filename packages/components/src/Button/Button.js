8130784166723c691c8ffa33ed08d356a022396f
Kelly Willard <kellyw@kellywiardmbp16.lan>
2020-07-27 10:23:38 -0700

    initial commit

---
+++ packages/components/src/Button/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+import Button from "@dtc-design-system/core/lib/Button";
+import Theme from "@dtc-design-system/core/lib/Theme";
+
+const StyledButton = styled(Button)`
+color: #03a9f4
+`
+
+const Button1 = ({ onClick, variant, primary, children }) => {
+  return (
+    <Theme>
+      <StyledButton onClick={onClick} primary={primary} variant={variant}>{children}</StyledButton>
+    </Theme>
+  );
+};
+
+export default Button1;
